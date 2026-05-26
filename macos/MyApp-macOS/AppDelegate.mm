060533cd781089d8935ff9d18b256836f15d2a5d
shirakaba <14055146+shirakaba@users.noreply.github.com>
2026-05-26 12:29:29 +0200

    Initial commit

---
+++ macos/MyApp-macOS/AppDelegate.mm
@@ -0,0 +1,50 @@
+#import "AppDelegate.h"
+
+#import <React/RCTBundleURLProvider.h>
+#import <ReactAppDependencyProvider/RCTAppDependencyProvider.h>
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)notification
+{
+  self.moduleName = @"main";
+  // You can add your custom initial props in the dictionary below.
+  // They will be passed down to the ViewController used by React Native.
+  self.initialProps = @{};
+  self.dependencyProvider = [RCTAppDependencyProvider new];
+  
+  [super applicationDidFinishLaunching:notification];
+// @generated begin expo-desktop-window-title - expo prebuild (DO NOT MODIFY) sync-f0c38cc89796c6c7797e486a51ecb20002985345
+  self.window.title = @"My App";
+// @generated end expo-desktop-window-title
+}
+
+- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
+{
+  return [self bundleURL];
+}
+
+- (NSURL *)bundleURL
+{
+#if DEBUG
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@".expo/.virtual-metro-entry"];
+#else
+  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
+}
+
+/// This method controls whether the `concurrentRoot`feature of React18 is turned on or off.
+///
+/// @see: https://reactjs.org/blog/2022/03/29/react-v18.html
+/// @note: This requires to be rendering on Fabric (i.e. on the New Architecture).
+/// @return: `true` if the `concurrentRoot` feature is enabled. Otherwise, it returns `false`.
+- (BOOL)concurrentRootEnabled
+{
+#ifdef RN_FABRIC_ENABLED
+  return true;
+#else
+  return false;
+#endif
+}
+
+@end
